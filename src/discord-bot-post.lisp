8417ceb51ba25298bf6abb20ec70670f0592a3ac
Yukko31415 <yukko31415@gmail.com>
2025-11-14 01:29:48 +0900

    引数の書き換えと実際に動くかどうかのテストコードを一緒に書いちゃってます

--- src/discord-bot-post.lisp
+++ src/discord-bot-post.lisp
@@ -2,19 +2,30 @@
 
 (in-package #:discord-bot-post)
 
-
-
+(defvar *bot-token*
+  (with-open-file (token (pathname "~/common-lisp/discord-bot/data/token.txt")
+			 :direction :input)
+    (read-line token))
+  "Discordボットのトークン")
 
 ;;;; ------------------------------------------------------------------
 ;;;; Discord API連携
 ;;;; ------------------------------------------------------------------
 
 
-(defun send-discord-message (embed-data)
+(defun make-header (bot-token content-type)
+  "bot-tokenとcontent-typeを受け取り、ヘッダーのリストを生成して返す"
+  (list `(#:Authorization . ,(format nil "Bot ~A" bot-token))
+	`(#:Content-Type . ,content-type)))
+
+
+(defun send-discord-message (embed-data channel-id bot-token)
+  
   "Discordの指定されたチャンネルにEmbedメッセージを送信する"
-  (let* ((url (format nil "https://discord.com/api/v10/channels/~A/messages" *channel-id*))
-	 (headers `(("Authorization" . ,(format nil "Bot ~A" *bot-token*))
-		    ("Content-Type" . "application/json")))
+  
+  (let* ((url (format nil "https://discord.com/api/v10/channels/~A/messages" channel-id))
+	 
+	 (headers (make-header bot-token "application/json"))
 
 	 ;; 1. 空のハッシュテーブルを作成
 	 (payload-ht (make-hash-table :test 'equal))
@@ -23,12 +34,15 @@
 		    (setf (gethash "embeds" payload-ht) (list embed-data))
 		    ;; 3. ハッシュテーブルをJSON文字列にエンコード
 		    (cl-json:encode-json-to-string payload-ht))))
-    
+
     (handler-case
-	(drakma:http-request url
-			     :method :post
-			     :additional-headers headers
-			     :content payload
-			     :content-type "application/json")
+	(dex:post url
+		  :headers headers
+		  :content payload)
       (error (e)
 	(format t "Discord送信エラー: ~A~%" e)))))
+
+(send-discord-message '((#:title . "hello!")) 1406525194289287311 (with-open-file (token (pathname "~/common-lisp/discord-bot/data/token.txt")
+			 :direction :input)
+    (read-line token)))
+
