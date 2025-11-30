5375c7614e246d550f4d2b836460b34ba03227e9
Yukko31415 <yukko31415@gmail.com>
2025-12-01 01:34:57 +0900

    boot関数/initialize関数の実装

--- src/discord-bot-main.lisp
+++ src/discord-bot-main.lisp
@@ -5,7 +5,27 @@
 
 
 ;;;; --------------------------------------------------------------
-;;;; start/end ----------------------------------------------------
+;;;; start/end
+;;;; --------------------------------------------------------------
+
+
+(defparameter *initialize-list* '(:token :rss))
+
+(defun initialize ()
+  "各パッケージのinitializeコマンドを実行する"
+  (mapc #'(lambda (key)
+	    (run-command (key :initialize)))
+	*initialize-list*))
+
+
+(defun boot ()
+  "各パッケージのbotを起動させる"
+  (run-command (:main :command-list
+		      (:main :command-dotimes 3 (:rss :post) (:main :sleep 300))
+		      (:rss :save-queue))))
+
+
+
 
 
 (defun random-in-range (min max)
@@ -14,11 +34,6 @@
 
 (defun start ()
   "botを起動する。スレッドを作成し、boot関数を叩く"
-  (slynk:create-server :port (random-in-range 1000 65535) :dont-close t)
-  (join-thread (make-thread
-		#'(lambda () (boot)))))
-
-
-(defun boot ())
-
-
+  (progn (initialize)
+	 (boot)
+	 (slynk:create-server :port (random-in-range 1000 65535) :dont-close t)))
