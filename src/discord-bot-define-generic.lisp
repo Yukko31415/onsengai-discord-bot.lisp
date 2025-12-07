41d9f445cc7736c8bddad298494df2f91b5e63fe
Yukko31415 <yukko31415@gmail.com>
2025-12-08 01:00:30 +0900

    bot-command/run-commandの実装を変更

--- src/discord-bot-define-generic.lisp
+++ src/discord-bot-define-generic.lisp
@@ -6,65 +6,65 @@
 ;;;; generic関数の定義
 ;;;; --------------------------------------------------------------
 
-(defgeneric bot-command (bot-type command arg))
+(defgeneric bot-command (bot-type command &rest arg))
 
 ;;;; --------------------------------------------------------------
 ;;;; main-command
 ;;;; --------------------------------------------------------------
 
+
+(defun transform-command-syntax (x)
+  (if (consp x)
+      (typecase (car x)
+	(keyword `(list ,@(mapcar #'transform-command-syntax x)))
+	(symbol x)
+	(t `',x))
+      x))
+
+
+(defmacro run-command (command)
+  (destructuring-bind (bot-type command &rest arg) command
+    (let ((arg-list (mapcar #'transform-command-syntax arg)))
+      `(bot-command ,bot-type ,command ,@arg-list))))
+
+
 (defmacro defcommand (bot-type command arg-let &body body)
   (let ((type (gensym))
 	(key (gensym)))
-    `(defmethod bot-command ((,type (eql ,bot-type)) (,key (eql ,command)) ,arg-let)
+    `(defmethod bot-command
+	 ((,type (eql ,bot-type)) (,key (eql ,command)) &rest ,arg-let)
        ,@body)))
 
 
-(defun make-command-list (lst)
-  (cons 'list
-	(mapcar #'(lambda (arg)
-		    (if (listp arg)
-			(if (listp (cdr arg))
-			    (make-command-list arg)
-			    `(cons ,(car arg) ,(cdr arg)))
-			(identity arg)))
-		lst)))
-
-
-
-(defmacro run-command (list)
-  (let ((bot-type (first list))
-	(command (second list))
-	(arg (make-command-list (cddr list))))
-    `(bt:make-thread
-      #'(lambda () (bot-command ,bot-type ,command ,arg)))))
-
-
-
 (defcommand :main :sleep arg
-  (progn (format t "sleep ~a~%" (car arg))
-	 (sleep (car arg))))
+  (sleep (car arg)))
 
 (defcommand :main :command-list arg
   (dolist (i arg)
-    (let ((bot-type (first i))
-	  (command (second i))
-	  (arg (cddr i)))
-      (bot-command bot-type command arg))))
+    (destructuring-bind (bot-type command &rest arg) i 
+      (apply #'bot-command bot-type command arg))))
 
+(defcommand :main :with-thread arg
+  (destructuring-bind (bot-type command &rest arg) (car arg)
+    (bt:make-thread
+     #'(lambda ()
+       (apply #'bot-command bot-type command arg)))))
 
 (defcommand :main :command-dotimes arg
-  (let ((dotime (car arg))
-	(command-list (cdr arg)))
-    (dotimes (i dotime)
-      (bot-command :main :command-list command-list))))
-
+  (destructuring-bind (dotime command-list) arg
+      (dotimes (i dotime)
+	(bot-command :main :command-list command-list))))
 
 (defun make-loop-list (list)
   (setf (cdr (last list)) list))
+
 
 (defcommand :main :loop-command arg
   (let ((looped-command-list (make-loop-list arg)))
     (bot-command :main :command-list looped-command-list)))
 
+
+(defcommand :main :test num
+  (format t "this is test. num is ~a~%" (car num)))
 
 
