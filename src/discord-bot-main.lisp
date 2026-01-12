cb5ee4fb662c45d26ee27c548b7dc76e191d535a
Yukko31415 <yukko31415@gmail.com>
2026-01-12 22:35:01 +0900

    end関数を削除

--- src/discord-bot-main.lisp
+++ src/discord-bot-main.lisp
@@ -31,10 +31,10 @@
   "Generates a random integer within the range [min, max]."
   (+ min (random (1+ (- max min)))))
 
+
 (defun start ()
   "botを起動する。スレッドを作成し、boot関数を叩く"
   (progn (initialize)
 	 (boot)
 	 (slynk:create-server :port (random-in-range 1000 65535) :dont-close t)))
 
-(defun end ())
